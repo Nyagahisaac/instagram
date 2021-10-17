aa7816ede362b853e4a8817811bac5776b0e636e
Nyagahisaac <isaac.ireri@student.moringaschool.com>
2021-10-18 00:37:40 +0300

    add my new post functionality in my iews.py file

--- instagram/views.py
+++ instagram/views.py
@@ -40,3 +40,21 @@ def profile(request):
         user=current_user
         profile =Profile.get_profile(user)
         return render(request, 'profile.html', {'profile': profile, 'title': title, 'form': form})
+
+
+@login_required(login_url='/accounts/login/')
+def new_post(request):
+    current_user = request.user
+    if request.method == 'POST':
+        form = NewImageForm(request.POST, request.FILES)
+        if form.is_valid():
+            image = form.save(commit=False)
+            image.user = current_user
+            image.save()
+            # article.post = strip_tags(request.POST['post'])
+            # article.save()
+        return redirect('welcome')
+
+    else:
+        form = NewImageForm()
+    return render(request, 'new-post.html', {"form": form})
